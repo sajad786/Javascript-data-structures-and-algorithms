692967fa61548d890bd23b44eb5fcd6f6145bf2b
Sajad Hussain <sajad1820375@gmail.com>
2022-02-05 23:58:03 +0530

    js sitrings

---
+++ strings.js
@@ -0,0 +1,21 @@
+// accessing strings ********************
+'dog'.charAt(1);
+console.log('dog'.charAt(1))
+
+// SUbstring ************************
+
+var some = "SajadHusain.jpg";
+
+var someNew = some.substring(some.length - 3, some.length);
+
+// console.log(someNew)
+
+// console.log(some.substring(1))
+
+// string Comparision ********************************
+
+var a = "someOne";
+
+var b = "someTwo";
+
+console.log(a < b); // true
